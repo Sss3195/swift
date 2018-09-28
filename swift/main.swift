26f829410de3e37ceb7ad1605bef598fcd7a296d
s20171103195 <s20171103195@danqianfang.local>
2018-09-28 20:11:40 +0800

    Initial Commit

---
+++ swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift
+//
+//  Created by s20171103195 on 2018/9/28.
+//  Copyright © 2018年 s20171103195. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
